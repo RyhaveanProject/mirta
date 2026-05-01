f0e7ba03fa899c8da39e5e9a9b5669f74da17a87
RyhaveanProject <bfnsscxj97@privaterelay.appleid.com>
2026-05-02 00:16:45 +0400

    Fix escape characters in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+"import React from \"react\";
+import ReactDOM from \"react-dom/client\";
+import \"./index.css\";
+import App from \"./App\";
 
-const link = document.createElement("link");
-link.rel = "stylesheet";
-link.href = "https://fonts.googleapis.com/css2?family=Sora:wght@400;600;700;800&family=Outfit:wght@400;600;700&family=JetBrains+Mono:wght@400;500&display=swap";
+const link = document.createElement(\"link\");
+link.rel = \"stylesheet\";
+link.href = \"https://fonts.googleapis.com/css2?family=Sora:wght@400;600;700;800&family=Outfit:wght@400;600;700&family=JetBrains+Mono:wght@400;500&display=swap\";
 document.head.appendChild(link);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById(\"root\"));
 root.render(
   <React.StrictMode>
     <App />
