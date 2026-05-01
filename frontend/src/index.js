065f0d405aa096eb3332c008a449b60ee29b636b
RyhaveanProject <bfnsscxj97@privaterelay.appleid.com>
2026-05-01 19:32:38 +0400

    Wrap App component in React.StrictMode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-// Premium fonts
 const link = document.createElement("link");
 link.rel = "stylesheet";
 link.href = "https://fonts.googleapis.com/css2?family=Sora:wght@400;600;700;800&family=Outfit:wght@400;600;700&family=JetBrains+Mono:wght@400;500&display=swap";
 document.head.appendChild(link);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
